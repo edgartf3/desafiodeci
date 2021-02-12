76442b1d8edce2344308c909942ec573a23dcedb
Edgar <edgartorresfreitas@gmail.com>
2021-02-12 08:41:20 -0300

    primeiro commit

---
+++ main.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func soma(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(soma(5, 5))
+}
